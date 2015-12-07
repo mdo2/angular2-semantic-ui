37dbe43db1e906c887a5d3ac026e2204a56f4661
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2015-12-07 18:21:53 +0100

    Clean exampler folder and typescript compiler fixes

---
+++ src/app/components/dropdown/DropdownComponent.ts
@@ -0,0 +1,14 @@
+/*
+ * Dropdown  Semantic UI component
+ */
+import { Directive, Component, View, ElementRef } from "angular2/angular2";
+
+@Component({
+  selector: "sc-dropdown",
+  directives: [],
+  styles: [],
+  template: ``
+})
+export class DropdownComponent {
+
+}
