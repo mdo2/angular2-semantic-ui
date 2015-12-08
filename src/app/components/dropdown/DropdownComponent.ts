c648c566c68da633a283702390ccc105559d3b43
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2015-12-08 14:49:43 +0100

    Add SemanticComponentDirective

--- src/app/components/dropdown/DropdownComponent.ts
+++ src/app/components/dropdown/DropdownComponent.ts
@@ -2,32 +2,39 @@
  * Dropdown  Semantic UI component
  */
 import { Directive, Component, View, ElementRef } from "angular2/angular2";
+import { SemanticComponentDirective } from "../../directives/SemanticComponent";
 
 @Component({
   selector: "sc-dropdown",
-  directives: [],
+  directives: [ SemanticComponentDirective ],
   styles: [],
   template: `
-  <div class="ui selection dropdown">
+  <div sc-component sc-name="dropdown" [sc-config]="scConfig" class="ui selection dropdown">
     <input type="hidden" name="gender">
     <i class="dropdown icon"></i>
     <div class="default text">Gender</div>
     <div class="menu">
-      <div class="item" data-value="1">Male</div>
-      <div class="item" data-value="0">Female</div>
+      <div class="item" data-value="male">Male</div>
+      <div class="item" data-value="female">Female</div>
     </div>
   </div>
+  <span>Value: {{ value }}</span>
   `
 })
 export class DropdownComponent {
+
+  //Semantic Component
+  scConfig: Object = {};
+
+  //Properties
   $elem: any;
-  dropdown: any;
+  value: any;
 
   constructor(elem: ElementRef){
     this.$elem = $(elem.nativeElement);
   }
 
-  onInit(){
-    this.dropdown = this.$elem.find(".ui.dropdown").dropdown();
-  }
+  // onInit(){
+  // 
+  // }
 }
