30187ee47aae0e195dfdb83b4538c79ff8349a27
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2015-12-09 18:23:03 +0100

    Add site navbar component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,42 +1,29 @@
 /*
- * Angular 2 decorators and services
- */
-import {Directive, Component, View} from "angular2/angular2";
-import { DropdownComponent } from "./components/dropdown/DropdownComponent";
-
-/*
  * App Component
  * Top Level Component
  */
+
+import {Directive, Component, View} from "angular2/angular2";
+
+import { SiteNavbarComponent } from "./site/components/navbar/navbar";
+
+import { DropdownComponent } from "./components/dropdown/DropdownComponent";
+
+
 @Component({
-  // The selector is what angular internally uses
-  // for `document.querySelectorAll(selector)` in our index.html
-  // where, in this case, selector is the string "app"
-  selector: "sc-app", // <app></app>
-  // We need to tell Angular"s compiler which directives are in our template.
-  // Doing so will allow Angular to attach our behavior to an element
+  selector: "sc-app",
   directives: [
+    SiteNavbarComponent,
     DropdownComponent
   ],
-  // Our list of styles in our component. We may add more to compose many styles together
   styles: [`
     .item .icon:first-child:last-child{
       margin: 0px;
     }
   `],
-  // Every Angular template is first compiled by the browser before Angular runs it"s compiler
   template: `
-  <header class="red">
-    <div class="ui red inverted menu">
-      <div class="header item">
-        Angular 2 Semantic UI Components
-      </div>
-      <div class="right menu">
-        <a class="item white" target="_blank" href="https://github.com/mdo2/angular2-semantic-ui">
-          <i class="github icon large"></i>
-        </a>
-      </div>
-    </div>
+  <header>
+    <sc-site-navbar></sc-site-navbar>
   </header>
 
   <main>
---
+++ src/app/site/components/navbar/navbar.ts
@@ -0,0 +1,15 @@
+/*
+ * Site Navbar component
+ */
+import { Directive, Component, View } from "angular2/angular2";
+
+@Component({
+  selector: "sc-site-navbar"
+})
+@View({
+  directives: [],
+  styles: [],
+  templateUrl: "components/navbar/navbar.html"
+})
+export class SiteNavbarComponent {
+}
